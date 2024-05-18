227d40e1b62138fec8e7f91590a8f4aa6d6667c2
Sudhanshu Shekhar <161944120+Sudhanshus25@users.noreply.github.com>
2024-05-18 09:47:02 +0530

    Add files via upload

---
+++ CodeAlpha_Age_Calculator/script.js
@@ -0,0 +1,25 @@
+function calculateAge() {
+    var dobDate = new Date(document.getElementById('dob').value);
+    var today = new Date();
+    var age = today.getFullYear() - dobDate.getFullYear();
+    var monthDiff = today.getMonth() - dobDate.getMonth() - 1;
+    var dayDiff = today.getDate() - dobDate.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+        monthDiff += 12;
+    }
+
+    if (dayDiff < 0) {
+        var lastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+        dayDiff = lastMonth.getDate() - dobDate.getDate() + today.getDate() + 1;
+    }
+
+    document.getElementById('years').textContent = age;
+    document.getElementById('months').textContent = monthDiff;
+    document.getElementById('days').textContent = dayDiff;
+}
+
+document.getElementById('calculate').addEventListener("click", calculateAge);
+
+
